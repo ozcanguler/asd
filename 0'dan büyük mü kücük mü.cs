6d83c0921e29ca1302d400c10de69cf7c457e8b4
ozcanguler <77888763+ozcanguler@users.noreply.github.com>
2021-04-15 17:48:34 +0300

    Add files via upload

---
+++ 0'dan büyük mü kücük mü.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double num;
+            Console.Write("Enter the number pls:");
+            num = Convert.ToDouble(Console.ReadLine());
+            if (num < 0) { Console.WriteLine("number less than Zero"); }
+            else if (num > 0) { Console.WriteLine("number greater than Zero"); }
+            else Console.WriteLine("number equal to Zero");
+        }
+    }
+}
---
+++ Faktoriyel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num,fac=1;
+            Console.WriteLine("Enter the factorial number");
+            num = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i < num; i++)
+            {
+                fac = fac*(i+1);
+               
+            }
+            Console.WriteLine("Factorial number:"+fac);
+        }
+        
+    }
+}
