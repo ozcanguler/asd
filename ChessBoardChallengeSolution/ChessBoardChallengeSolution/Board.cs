d1538f293c3543511cb4f43ff984e7b3a5ea6d9b
OzcanGuler <gulerozcan818@gmail.com>
2022-07-20 00:25:29 +0300

    Keep Updating

---
+++ ChessBoardChallengeSolution/ChessBoardChallengeSolution/Board.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessBoardChallengeSolution
+{
+    public class Board
+    {
+        public int Size { get; set; }   //8x8
+        public Cell[,] theGrid { get; set; }
+
+        public Board(int s)
+        {
+            Size = s;
+            theGrid = new Cell[Size, Size];
+
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    theGrid[i, j] = new Cell(i, j);
+                }
+            }
+        }
+        public bool isSafe(int x, int y)
+        {
+            if (x < 0 || x >= Size || y < 0 || y >= Size)
+            {
+                return false;
+            }
+            else return true;
+        }
+        public void MarkNextLegalMoves(Cell currentcell, string chessPiece)
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    theGrid[i, j].LegalNextMove = false;
+                    theGrid[i, j].CurrentlyOccupied = false;
+                }
+            }
+            switch (chessPiece)
+            {
+                case "Knight":
+                    if (isSafe(currentcell.RowNumber + 2, currentcell.ColumnNumber + 1))
+                        theGrid[currentcell.RowNumber + 2, currentcell.ColumnNumber + 1].LegalNextMove = true;
+                    if (isSafe(currentcell.RowNumber + 1, currentcell.ColumnNumber + 2))
+                    theGrid[currentcell.RowNumber + 1, currentcell.ColumnNumber + 2].LegalNextMove = true;
+
+                    if (isSafe(currentcell.RowNumber - 2, currentcell.ColumnNumber - 1))
+                        theGrid[currentcell.RowNumber -2, currentcell.ColumnNumber -1].LegalNextMove = true;
+
+                        if (isSafe(currentcell.RowNumber - 1, currentcell.ColumnNumber - 2))
+                        theGrid[currentcell.RowNumber - 1, currentcell.ColumnNumber - 2].LegalNextMove = true;
+
+                            if (isSafe(currentcell.RowNumber + 2, currentcell.ColumnNumber - 1))
+                        theGrid[currentcell.RowNumber +2, currentcell.ColumnNumber - 1].LegalNextMove = true;
+
+                                if (isSafe(currentcell.RowNumber + 1, currentcell.ColumnNumber - 2))
+                        theGrid[currentcell.RowNumber + 1, currentcell.ColumnNumber - 2].LegalNextMove = true;
+
+                                    if (isSafe(currentcell.RowNumber - 2, currentcell.ColumnNumber + 1))
+                        theGrid[currentcell.RowNumber -2, currentcell.ColumnNumber +1].LegalNextMove = true;
+
+                                        if (isSafe(currentcell.RowNumber - 1, currentcell.ColumnNumber + 2))
+                        theGrid[currentcell.RowNumber - 1, currentcell.ColumnNumber + 2].LegalNextMove = true;
+                    break;
+
+                case "King":
+                    if(isSafe( currentcell.RowNumber + 1, currentcell.ColumnNumber))
+                    theGrid[currentcell.RowNumber + 1, currentcell.ColumnNumber ].LegalNextMove = true;
+
+                    if(isSafe( currentcell.RowNumber + 1, currentcell.ColumnNumber + 1))
+                    theGrid[currentcell.RowNumber + 1, currentcell.ColumnNumber + 1].LegalNextMove = true;
+
+                    if (isSafe(currentcell.RowNumber + 1, currentcell.ColumnNumber - 1))
+                        theGrid[currentcell.RowNumber + 1, currentcell.ColumnNumber - 1].LegalNextMove = true;
+
+                    if (isSafe(currentcell.RowNumber, currentcell.ColumnNumber - 1))
+                        theGrid[currentcell.RowNumber , currentcell.ColumnNumber - 1].LegalNextMove = true;
+
+                    if (isSafe(currentcell.RowNumber, currentcell.ColumnNumber + 1))
+                        theGrid[currentcell.RowNumber , currentcell.ColumnNumber + 1].LegalNextMove = true;
+
+                    if (isSafe(currentcell.RowNumber - 1, currentcell.ColumnNumber - 1))
+                        theGrid[currentcell.RowNumber - 1, currentcell.ColumnNumber - 1].LegalNextMove = true;
+
+                    if (isSafe(currentcell.RowNumber - 1, currentcell.ColumnNumber))
+                        theGrid[currentcell.RowNumber - 1, currentcell.ColumnNumber ].LegalNextMove = true;
+
+                    if (isSafe(currentcell.RowNumber - 1, currentcell.ColumnNumber + 1))
+                        theGrid[currentcell.RowNumber - 1, currentcell.ColumnNumber + 1].LegalNextMove = true;
+
+                    break;
+                case "Rook":
+                    for (int i = 0; i < 8; i++)
+                    {
+                        if (isSafe(currentcell.RowNumber - i, currentcell.ColumnNumber))
+                            theGrid[currentcell.RowNumber - i, currentcell.ColumnNumber].LegalNextMove = true;
+
+                        if (isSafe(currentcell.RowNumber +i , currentcell.ColumnNumber))
+                            theGrid[currentcell.RowNumber + i, currentcell.ColumnNumber].LegalNextMove = true;
+
+                        if (isSafe(currentcell.RowNumber , currentcell.ColumnNumber - i))
+                            theGrid[currentcell.RowNumber , currentcell.ColumnNumber - i].LegalNextMove = true;
+
+                        if (isSafe(currentcell.RowNumber , currentcell.ColumnNumber + i))
+                            theGrid[currentcell.RowNumber , currentcell.ColumnNumber + i].LegalNextMove = true;
+                    }
+                    
+                    break;
+                case "Bishop":
+
+                    for (int i = 0; i < 8; i++)
+                    {
+                        if (isSafe(currentcell.RowNumber - i, currentcell.ColumnNumber - i))
+                            theGrid[currentcell.RowNumber - i, currentcell.ColumnNumber - i].LegalNextMove = true;
+
+                        if (isSafe(currentcell.RowNumber + i, currentcell.ColumnNumber + i))
+                            theGrid[currentcell.RowNumber + i, currentcell.ColumnNumber + i].LegalNextMove = true;
+
+                        if (isSafe(currentcell.RowNumber + i, currentcell.ColumnNumber - i))
+                            theGrid[currentcell.RowNumber + i, currentcell.ColumnNumber - i].LegalNextMove = true;
+
+                        if (isSafe(currentcell.RowNumber - i, currentcell.ColumnNumber + i))
+                            theGrid[currentcell.RowNumber - i, currentcell.ColumnNumber + i].LegalNextMove = true;
+
+                    }
+
+                    break;
+                case "Queen":
+
+                    for (int i = 0; i < 8; i++)
+                    {
+                        if (isSafe(currentcell.RowNumber - i, currentcell.ColumnNumber - i))
+                            theGrid[currentcell.RowNumber - i, currentcell.ColumnNumber - i].LegalNextMove = true;
+
+                        if (isSafe(currentcell.RowNumber + i, currentcell.ColumnNumber + i))
+                            theGrid[currentcell.RowNumber + i, currentcell.ColumnNumber + i].LegalNextMove = true;
+
+                        if (isSafe(currentcell.RowNumber + i, currentcell.ColumnNumber - i))
+                            theGrid[currentcell.RowNumber + i, currentcell.ColumnNumber - i].LegalNextMove = true;
+
+                        if (isSafe(currentcell.RowNumber - i, currentcell.ColumnNumber + i))
+                            theGrid[currentcell.RowNumber - i, currentcell.ColumnNumber + i].LegalNextMove = true;
+
+                        if (isSafe(currentcell.RowNumber - i, currentcell.ColumnNumber))
+                            theGrid[currentcell.RowNumber - i, currentcell.ColumnNumber].LegalNextMove = true;
+
+                        if (isSafe(currentcell.RowNumber + i, currentcell.ColumnNumber))
+                            theGrid[currentcell.RowNumber + i, currentcell.ColumnNumber].LegalNextMove = true;
+
+                        if (isSafe(currentcell.RowNumber, currentcell.ColumnNumber - i))
+                            theGrid[currentcell.RowNumber, currentcell.ColumnNumber - i].LegalNextMove = true;
+
+                        if (isSafe(currentcell.RowNumber, currentcell.ColumnNumber + i))
+                            theGrid[currentcell.RowNumber, currentcell.ColumnNumber + i].LegalNextMove = true;
+
+                    }
+                    break;
+            }
+            theGrid[currentcell.RowNumber, currentcell.ColumnNumber].CurrentlyOccupied = true;
+        }
+
+    }
+}
---
+++ ChessBoardChallengeSolution/ConsoleApp1/Program.cs
@@ -0,0 +1,109 @@
+﻿using ChessBoardChallengeSolution;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static Board myBoard = new Board(8);
+
+        static void Main(string[] args)
+        {
+            printBoard(myBoard);
+            Cell currentCell = setCurrentCell();          
+            currentCell.CurrentlyOccupied = true;
+            myBoard.MarkNextLegalMoves(currentCell, setPiece());
+            printBoard(myBoard);
+        }
+
+        private static Cell setCurrentCell()
+        {
+            Console.WriteLine("Enter the current row number");
+            int currentRow = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the current column number");
+            int currentColumn = int.Parse(Console.ReadLine());
+
+          
+
+            return myBoard.theGrid[currentRow, currentColumn];
+        }
+        private static string setPiece()
+        {
+            Console.WriteLine("What kind of chess piece would you like to place on the board? Knight, King, Queen, Bishop, Rook");
+            string piece = Console.ReadLine();
+            return piece;
+        }
+
+
+        //private static void printBoard(Board myBoard)
+        //{
+        //    for (int i = 0; i < myBoard.Size; i++)
+        //    {
+        //        for (int j = 0; j < myBoard.Size; j++)
+        //        {
+        //            Cell c = myBoard.theGrid[i, j];
+
+        //            if (c.CurrentlyOccupied == true)
+        //            {
+        //                Console.Write("X");
+        //            }
+        //            else if (c.LegalNextMove == true)
+        //            {
+        //                Console.Write("+");
+        //            }
+        //            else
+        //            {
+        //                Console.Write(".");
+        //            }
+        //        }
+        //        Console.WriteLine();
+        //    }
+        //    Console.WriteLine("=======================");
+
+        //}
+
+        private static void printBoard(Board myBoard)
+        {
+            for (int i = 0; i < myBoard.Size; i++)
+            {
+                for (int b = 0; b < myBoard.Size; b++)
+                {
+                    Console.Write("+---");
+                }
+                Console.Write("+");
+                Console.WriteLine();
+                Console.Write("| ");
+                for (int j = 0; j < myBoard.Size; j++)
+                {
+
+
+                    Cell c = myBoard.theGrid[i, j];
+
+                    if (c.CurrentlyOccupied == true)
+                    {
+                        Console.Write("X");
+                    }
+                    else if (c.LegalNextMove == true)
+                    {
+                        Console.Write("+");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write(" | ");
+                }
+                Console.WriteLine();
+
+            }
+            for (int d = 0; d < myBoard.Size; d++)
+            {
+                Console.Write("+---");
+            }
+            Console.Write("+");
+            Console.WriteLine();
+            Console.WriteLine("=======================");
+
+        }
+    }
+}
